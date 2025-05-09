18c56e3ab863db534f4ea9c4f09e2ac0880f1fc0
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-05-09 05:29:52 -0700

    Delete pluralsight_R directory

--- pluralsight_R/Hello_World.R
+++
@@ -1,5 +0,0 @@
-# Assign a Character string to a variable
-x <- "Hello World!"
-
-# Print the variable
-print(x)
--- pluralsight_R/Working_With_Data.R
+++
@@ -1,4 +0,0 @@
-# Working with Data
-
-# Set the working directory 
-setwd("C:/Users/evenrichardson/dev/r-challenges/Working_With_Data")
